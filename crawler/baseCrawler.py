b86f7cb5e13dd83086da8e99377c68d0ec36b2a7
jim <326695231@qq.com>
2018-12-18 08:05:35 +0800

    norvalCrawler

--- crawler/baseCrawler.py
+++ crawler/baseCrawler.py
@@ -1,7 +1,11 @@
 class BaseCrawler:
 
     def __init__(self):
-        self.headers = {
+        pass
+
+    @property
+    def headers(self):
+        return {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
             'Accept-Encoding': 'gzip, deflate, sdch',
             'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
